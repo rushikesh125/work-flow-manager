fd67769fe559d2eff56f1877d9c198ba2711d899
rushikesh125 <lolits2626@gmail.com>
2025-09-10 23:06:03 +0530

    shadcn ui added

--- src/components/DasbboardWrapper.tsx
+++ src/components/DasbboardWrapper.tsx
@@ -4,9 +4,9 @@ import React, { ReactNode } from 'react'
 
 const DashboardWrapper = ({children}:{children:ReactNode}) => {
   return (
-    <div className='flex'>
-        <div>Siderbar</div>
-        <div>Navbar</div>
+    <div className='flex w-full bg-gray-50 min-h-screen text-gray-900'>
+        <div className=''>Siderbar</div>
+        <main className='dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64'>Navbar</main>
         <div>{children}</div>
     </div>
 
